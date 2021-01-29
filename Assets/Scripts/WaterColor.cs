85866b9692caa9aa38495c560ad8747e55c87c8e
momo1478 <monish1478@gmail.com>
2021-01-29 03:37:30 -0800

    Submarine Stuff

---
+++ Assets/Scripts/WaterColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterColor : MonoBehaviour
+{
+    [Range(1,200)]
+    public float MaxHeight = 100f;
+
+    public Gradient Gradient;
+
+    Terrain terrain;
+    Camera camera;
+
+    float initialTerrainHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GameObject.Find("Sea Floor").GetComponent<Terrain>();
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Mathf.InverseLerp(0, MaxHeight, transform.position.y - terrain.SampleHeight(transform.position));
+
+        camera.backgroundColor = Gradient.Evaluate(1 - t);
+        RenderSettings.fogColor = Color.Lerp(Gradient.Evaluate(t), Color.black, t);
+    }
+}
