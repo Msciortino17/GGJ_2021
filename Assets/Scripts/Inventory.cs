2995abbbfe007fe6c2e34d4fb77310e30ce78cc4
Mike <msciortino17@gmail.com>
2021-01-29 16:37:21 -0500

    Setup a testing scene for me, and also laid groundwork for an inventory and artifact system

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	public delegate void SimpleCallback();
+	public SimpleCallback CollectedArtifactCallback;
+
+	public int NumArtifacts;
+	public bool[] CollectedArtifacts;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		CollectedArtifacts = new bool[NumArtifacts];
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			CollectArtifact(0);
+		}
+		if (Input.GetKeyDown(KeyCode.N))
+		{
+			CollectArtifact(1);
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			CollectArtifact(2);
+		}
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			CollectArtifact(3);
+		}
+	}
+
+	public bool HasArtifact(int _index)
+	{
+		if (_index >= 0 && _index < NumArtifacts)
+		{
+			return CollectedArtifacts[_index];
+		}
+		return false;
+	}
+
+	public void CollectArtifact(int _index)
+	{
+		if (_index >= 0 && _index < NumArtifacts)
+		{
+			CollectedArtifacts[_index] = true;
+			CollectedArtifactCallback?.Invoke();
+		}
+	}
+}
---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+	[SerializeField] private Inventory playerInventory;
+
+	public GameObject[] ArtifactIcons;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Init();
+	}
+
+	/// <summary>
+	/// Bind the callback for refreshing the UI, and turn off all icons to start.
+	/// </summary>
+	private void Init()
+	{
+		playerInventory.CollectedArtifactCallback += RefreshArtifactIcons;
+		for (int i = 0; i < ArtifactIcons.Length; i++)
+		{
+			ArtifactIcons[i].SetActive(false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	/// <summary>
+	/// We want to read which artifacts have been collected, and turn on the appropriate icon.
+	/// </summary>
+	private void RefreshArtifactIcons()
+	{
+		for (int i = 0; i < playerInventory.NumArtifacts; i++)
+		{
+			ArtifactIcons[i].SetActive(playerInventory.HasArtifact(i));
+		}
+	}
+}
