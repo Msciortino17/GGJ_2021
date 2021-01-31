c0d0cb2800838d9903a2141a63b8584aa3ecc20a
Taylor Famiglietti <MichaelTFamiglietti@gmail.com>
2021-01-31 02:31:30 -0600

    Finalized tentacle attack

---
+++ Assets/Scripts/Enemy/TentacleHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class TentacleHit : MonoBehaviour
+{
+    public float Damage = 20.0f;
+
+    public float Cooldown = 2.0f;
+
+    private void Update()
+    {
+        CooldownTimer.Interval();
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        Submarine submarine = other.GetComponent<Submarine>();
+        if( submarine != null )
+        {
+            if(CooldownTimer.Seconds >= Cooldown)
+            {
+                other.GetComponent<Submarine>()?.TakeDamage( Damage );
+                CooldownTimer.Reset();
+            }
+        }
+    }
+
+    private Timer CooldownTimer = new Timer();
+}
