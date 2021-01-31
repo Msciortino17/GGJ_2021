54fd2ae6fe98988831c315e7204cde6421f94348
RP <robartpond@gmail.com>
2021-01-30 22:04:15 -0500

    crash sounds added

--- Assets/Scripts/Enemy/SharkBite.cs
+++ Assets/Scripts/Enemy/SharkBite.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SharkBite : MonoBehaviour
 {
     public float Damage = 10.0f;
+    private AudioSource damageAudio;
+    public GameObject[] DmgPrefabs;
+    private int currentClip = 0;
 
     private void OnTriggerEnter(Collider other) {
         Submarine submarine = other.GetComponent<Submarine>();
         if(submarine != null)
         {
+            Transform dmg = Instantiate(DmgPrefabs[currentClip]).transform;
+            dmg.position = transform.position;
+            currentClip = 1 - currentClip;
             other.GetComponent<Submarine>()?.TakeDamage( Damage );
         }
     }
